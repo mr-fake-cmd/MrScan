1b29f3e90942599cc31aa3a9bf87f0f596c387f0
mr-fake-cmd <mr.fake.jarif@gmail.com>
2026-08-08 11:48:11 +0600

    Add files via upload

---
+++ mrscan.py
@@ -0,0 +1,11 @@
+from core.banner import banner
+from core.loading import loading
+from core.menu import menu
+
+def main():
+    banner()
+    loading()
+    menu()
+
+if __name__ == "__main__":
+    main()
